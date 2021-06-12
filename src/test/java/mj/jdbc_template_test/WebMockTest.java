554ab51bde0aa2f35888846f87a03311f39f10b1
MJbae <gently.gogo@gmail.com>
2021-06-12 11:09:01 +0900

    test: 특정 사용자 조회에 대한 mock test 구현

--- src/test/java/mj/jdbc_template_test/WebMockTest.java
+++ src/test/java/mj/jdbc_template_test/WebMockTest.java
@@ -1,15 +1,13 @@
 package mj.jdbc_template_test;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import mj.jdbc_template_test.domain.user.User;
-import mj.jdbc_template_test.service.GreetingService;
-import mj.jdbc_template_test.web.GreetingController;
+import mj.jdbc_template_test.service.UserService;
+import mj.jdbc_template_test.web.TestUserController;
+import mj.jdbc_template_test.web.dto.UserResponseDto;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-//@WebMvcTest(TestUserController.class)
-@WebMvcTest(GreetingController.class)
+@WebMvcTest(TestUserController.class)
 public class WebMockTest {
     private final static String FIRST_NAME_MJ = "M";
     private final static String LAST_NAME_MJ = "J";
@@ -36,29 +33,24 @@ public class WebMockTest {
     @Autowired
     private MockMvc mockMvc;
 
-//    @MockBean
-//    private UserService userservice;
-
     @MockBean
-    private GreetingService service;
+    private UserService userService;
 
     @Test
-    public void greetingShouldReturnMessageFromService() throws Exception {
-        when(service.greet()).thenReturn("Hello, Mock");
-        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello, Mock")));
+    public void 유저아이디_조건에_따라_특정_사용자_조회() throws Exception {
+        String expectByFirstName = "$.[?(@.firstName == '%s')]";
+        String expectByLastName = "$.[?(@.lastName == '%s')]";
+        String expectByIncome = "$.[?(@.yearlyIncome == '%s')]";
+
+        when(userService.findOneByUserId(GITHUB_ID_MJ)).thenReturn(
+                new UserResponseDto(new User(FIRST_NAME_MJ, LAST_NAME_MJ, GITHUB_ID_MJ, INCOME_MJ))
+        );
+
+        this.mockMvc.perform(get("/api/test_users/"+GITHUB_ID_MJ))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath(expectByFirstName, "M").exists())
+                .andExpect(jsonPath(expectByLastName, "J").exists())
+                .andExpect(jsonPath(expectByIncome, 10000000).exists());
+
     }
-
-
-//    @Test
-//    public void 유저아이디_조건에_따라_특정_사용자_조회() throws Exception {
-//
-//        when(service.findOneByUserId(GITHUB_ID_MJ)).thenReturn(
-//                new UserResponseDto(new User(FIRST_NAME_MJ, LAST_NAME_MJ, GITHUB_ID_MJ, INCOME_MJ))
-//        );
-//
-//        this.mockMvc.perform(get("/api/test_users/"+GITHUB_ID_MJ))
-//                .andDo(print());
-//
-//    }
 }
